bee78d33435dfcfb0222b375a551158f4061e122
Vaztha <santino.fassi@gmail.com>
2023-09-23 22:51:41 -0300

    funcion principal

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
 from PIL import Image, ImageTk
-
-ventana = None
-ruta = None
-
-def ajustarTamañoVentana(anchoImagen, altoImagen):
-    global ventana
-    # Asegurar que el tamaño mínimo de la ventana sea 800x600
-    anchoMinimo = max(800, anchoImagen + 100)
-    altoMinimo = max(600, altoImagen + 100)
-    
-    ventana.geometry(f"{anchoMinimo}x{altoMinimo}")
 
 # Definimos una función que recibe el nombre de una imagen y devuelve las coordenadas de los puntos extremos del área que no es blanca
 def obtenerExtremos(imagen: str) -> tuple:
@@ -42,7 +30,6 @@ def obtenerExtremos(imagen: str) -> tuple:
 
 # Definimos una función que recibe una imagen y las coordenadas de los puntos extremos del área que no es blanca y la mueve al borde inferior izquierdo de la imagen
 def moverImagen(imagen: str, puntos: tuple) -> None:
-    global ventana
     # Abrimos la imagen que nos pasan como argumento
     imagen = Image.open(imagen)
     ancho, alto = imagen.size
@@ -58,51 +45,26 @@ def moverImagen(imagen: str, puntos: tuple) -> None:
     auxY = alto - imagenCortada.height
     nuevaImagen.paste(imagenCortada, (auxX,auxY))
     nuevaImagen.save("nuevaImagen.png")
-
-    ventana.geometry(f"{ancho}x{alto}")
     
+    # Creamos una ventana de tkinter para mostrar la imagen modificada
+    ventana = tk.Tk()
+    ventana.title("Imagen con cuadrado negro")
+    ventana.geometry(f"{ancho}x{alto}")
+
     # Convertimos la imagen a formato PhotoImage para poder mostrarla en un widget de tkinter
     foto = ImageTk.PhotoImage(nuevaImagen)
 
     # Creamos un widget de tipo Label para mostrar la foto en la ventana
     etiqueta = tk.Label(ventana, image=foto)
-    etiqueta.image = foto
     etiqueta.pack()
-    
-    # Ajustar el tamaño de la ventana
-    ajustarTamañoVentana(imagenCortada.width(), imagenCortada.height())
 
-
-
-def abrirExplorador():
-    global ruta
-    # Abrir el explorador de archivos y obtener la ruta de la imagen seleccionada
-    rutaImagen = filedialog.askopenfilename(filetypes=[("Imágenes", "*.png *.jpg *.jpeg *.bmp")])
-    
-    if rutaImagen:
-        ruta = rutaImagen
-        # Obtenemos los puntos extremos del área que no es blanca de la imagen seleccionada
-        puntos = obtenerExtremos(ruta)
-        
-        # Movemos la imagen según los puntos obtenidos
-        moverImagen(ruta, puntos)
-    else:
-        ruta = -1
-    
-
-if __name__ == "__main__":
-    ventana = tk.Tk()
-    ventana.title("MESSI")
-    ventana.minsize(800,600)
-    ventana.resizable(True, True)
-    # Botón para abrir el explorador de archivos
-    botonAbrir = tk.Button(ventana, text="Abrir Imagen", command=abrirExplorador)
-    botonAbrir.pack()
-    
     # Iniciamos el bucle principal de la ventana
     ventana.mainloop()
     
-    if ruta is None:
-        print("No se seleccionó ninguna imagen")
-    elif ruta == -1:
-        print("Ruta no válida")
+
+if __name__ == "__main__":
+    # Obtenemos los puntos extremos del área que no es blanca de la imagen "imagen.png"
+    puntos = obtenerExtremos("imagen.png")
+    
+    # Movemos la imagen según los puntos obtenidos
+    moverImagen("imagen.png", puntos)
